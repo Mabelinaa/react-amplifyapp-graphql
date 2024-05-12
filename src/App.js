5e3e9fc3a89b5166f92d6bc323d697377f9b1f4b
Mabel Campomanes Isidoro <mcampomanes@iodigital.es>
2024-05-12 20:14:44 +0200

    added scripts AdminLTE

--- src/App.js
+++ src/App.js
@@ -1,148 +1,20 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import "@aws-amplify/ui-react/styles.css";
-import {
-  Button,
-  Flex,
-  Heading,
-  Image,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
-import { listTodos } from "./graphql/queries";
-import {
-  createTodo as createNoteMutation,
-  deleteTodo as deleteNoteMutation,
-} from "./graphql/mutations";
-
-import { generateClient } from "aws-amplify/api";
-import { uploadData, getUrl, remove } from 'aws-amplify/storage';
-
-
-const client = generateClient();
+import React from "react";
+import { withAuthenticator, ToggleButton } from "@aws-amplify/ui-react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import SideNav from "./components/SideNav";
 
 const App = ({ signOut }) => {
-  const [notes, setNotes] = useState([]);
-
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  async function fetchNotes() {
-    const apiData = await client.graphql({ query: listTodos });
-    const notesFromAPI = apiData.data.listNotes;
-    if (notesFromAPI){
-      await Promise.all(
-      notesFromAPI.map(async (note) =>{
-        if (note.image) {
-          const url = await getUrl({ key: note.name });
-          note.image = url.url;
-        }
-        return note
-      })
-    );
-    setNotes(notesFromAPI);
-    }
-  }
-
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-  
-    const name = form.get("name");
-  
-    if( name !== null){
-      const image = form.get("image");
-      const data = {
-        name: form.get("name"),
-        description: form.get("description"),
-
-      };
-      if (!!data.image) await uploadData({ key: data.name, data: image });
-      await client.graphql({
-        query: createNoteMutation,
-        variables: { input: data },
-      });
-      fetchNotes();
-      event.target.reset();
-    } else {
-      console.error("El nombre de la nota es null.");  
-    }
-  }
-  
-
-  async function deleteNote({ id, name }) {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-    await remove({ key: name });
-    await client.graphql({
-      query: deleteNoteMutation,
-      variables: { input: { id } },
-    });
-  }
 
   return (
-    <View className="App">
-      <Heading level={1}>My Notes App</Heading>
-      <View
-        name="image"
-        as="input"
-        type="file"
-        style={{ alignSelf: "end" }}
-      />
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            {note.image && (
-              <Image
-                src={note.image}
-                alt={`visual aid for ${notes.name}`}
-                style={{ width: 400 }}
-              />
-            )}
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
-          </Flex>
-        ))}
-      </View>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <div className="wrapper">
+      <Header/>
+      <Home/>
+      <SideNav/>
+      <Footer/>
+      <ToggleButton onClick={signOut}>Cerrar Sesión</ToggleButton>
+    </div>
   );
 };
 
